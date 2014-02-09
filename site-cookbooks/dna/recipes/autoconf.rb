867f5ef85f74c033e4381723bc4415387a0d1a70
Will Rossiter <will.rossiter@dna.co.nz>
2014-02-10 11:09:52 +1300

    Only if not exists

--- site-cookbooks/dna/recipes/autoconf.rb
+++ site-cookbooks/dna/recipes/autoconf.rb
@@ -2,8 +2,10 @@ brew "autoconf"
 
 execute "Link autoconf" do
 	command "ln -s /usr/local/Cellar/autoconf/2.69/bin/autoconf /usr/bin/autoconf"
+	not_if { ::File.exists?("/usr/bin/autoconf") }
 end
 
 execute "Link autoheader" do
 	command "ln -s /usr/local/Cellar/autoconf/2.69/bin/autoheader /usr/bin/autoheader"
+	not_if { ::File.exists?("/usr/bin/autoheader") }
 end
