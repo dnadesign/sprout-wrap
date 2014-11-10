53cd6d397131f54c3180b03ebae94e9023b9857b
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-11-10 13:44:11 +1300

    Correct capistrano installation

--- site-cookbooks/dna/recipes/capistrano.rb
+++ site-cookbooks/dna/recipes/capistrano.rb
@@ -1,9 +1,7 @@
-# gem_package causes error on our particular build on Gems
-# http://stackoverflow.com/questions/15328369/error-executing-action-install-on-resource-chef-gemmysql-installing-ruby
-execute "Install capistrano" do
-	command "gem install capistrano -v 2.15.5"
+gem_package 'capistrano' do
+  version '2.15.5'
 end
 
-execute "Install capistrano friends" do
-	command "gem install railsless-deploy capistrano_rsync_with_remote_cache"
-end
+gem_package 'railsless-deploy'
+
+gem_package 'capistrano_rsync_with_remote_cache'
